4b0e22afc449b75049f734e554f65fab65c82457
dzennank <dzenankrlic@gmail.com>
2022-12-06 13:25:28 +0100

    single listing page

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -18,7 +18,7 @@ class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"
     />
     <div>
         <h3 class="text-2xl">
-            <a href="show.html">{{$list->title}}</a>
+            <a href="listings/{{$list->id}}">{{$list->title}}</a>
         </h3>
         <div class="text-xl font-bold mb-4">{{$list->company}}</div>
         <ul class="flex">
